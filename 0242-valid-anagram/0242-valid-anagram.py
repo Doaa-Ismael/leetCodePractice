17f032ee2d109f9991070db43e73fbb059fb7696
Doaa Ismael <doaa.ismael@zyda.com>
2022-10-26 01:13:44 +0200

    Time: 52 ms (92.24%), Space: 14.4 MB (67.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        m1 = {}
-        m2 = {}
-        for l in s:
-            m1[l] = m1.get(l) + 1 if m1.get(l) is not None else 1 
-        for l in t:
-            m2[l] = m2.get(l) + 1 if m2.get(l) is not None else 1
+        s_map, t_map = {}, {}
+        for l in s: s_map[l] = s_map.get(l, 0) + 1
+        for l in t: t_map[l] = t_map.get(l, 0) + 1
         
-        if len(m1) != len(m2):
-            return False
-        
-        for c in m1:
-            if m1.get(c) != m2.get(c):
-                return False
-        return True
-        
+        return s_map == t_map
+      
         
