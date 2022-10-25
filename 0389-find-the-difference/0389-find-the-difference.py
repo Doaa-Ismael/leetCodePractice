02559d51f7cc583bf045329644c2d415ead2cae2
Doaa Ismael <doaa.ismael@zyda.com>
2022-10-26 01:34:26 +0200

    Time: 66 ms (50.01%), Space: 13.8 MB (76.08%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,9 +1,9 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s_map, t_map = {}, {}
-        for l in s: s_map[l] = s_map.get(l, 0) + 1
-        for l in t: t_map[l] = t_map.get(l, 0) + 1
-            
-        for l in t:
-            if t_map[l] > s_map.get(l, 0): return l
-    
+        res = 0
+       
+        for c in s: res ^= ord(c)
+        for c in t: res ^= ord(c)
+        
+        return chr(res)
+        
