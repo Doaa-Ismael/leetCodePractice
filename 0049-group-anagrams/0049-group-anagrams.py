a01954cec9465a9e9112b22fddb4835395a6ce1a
Doaa Ismael <doaa.ismael@zyda.com>
2022-11-05 22:31:15 +0200

    Time: 385 ms (5.17%), Space: 19.9 MB (9.88%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        words_map = {}
+        words_map = defaultdict(list)
         for s in strs:
-            soerted_word = ''.join(sorted(s))
-            if words_map.get(soerted_word):
-                words_map.get(soerted_word).append(s)
-            else:
-                words_map[soerted_word] = [s]
-
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+                
+            words_map[tuple(count)].append(s)
+    
         return words_map.values()
