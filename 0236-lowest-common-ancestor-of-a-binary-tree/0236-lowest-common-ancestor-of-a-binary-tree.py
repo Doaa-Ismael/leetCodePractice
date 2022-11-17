6ed13c1f27e1268227cff9b3a709daf2f9380146
Doaa Ismael <doaa.ismael@zyda.com>
2022-11-17 19:09:00 +0200

    Time: 158 ms (47.73%), Space: 26.4 MB (32.11%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,38 +7,21 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return
         
-        # 1) find the ancesstors of the two nodes and sort them and find the lowest one
-        #  find children of every node and if it contans p and q I will add it to the list
-        # find children of each node and if it has all 
+        if root == p or root == q:
+            return root
         
-        preorder1 = []
-        preorder2 = []
-        self.preorder(root, preorder1, p.val)
-        self.preorder(root, preorder2, q.val)
-        print([v.val for v in preorder1])
-        print([v.val for v in preorder2])
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
         
+        if left and not right:
+            return left
         
-        for a1 in preorder1:
-            for a2 in preorder2:
-                if a1.val == a2.val:
-                    return a1    
+        if right and not left:
+            return right
         
-        
-    def preorder(self, root, preorderList, n) -> None:
-        if not root:
-            return        
-        
-        if root.val == n:
-            preorderList.append(root)
-            return True
-        if self.preorder(root.left, preorderList, n):
-            preorderList.append(root)
-            return True
-            
-        if self.preorder(root.right, preorderList, n):
-            preorderList.append(root)  
-            return True
-        
+        if left and right:
+            return root
         
