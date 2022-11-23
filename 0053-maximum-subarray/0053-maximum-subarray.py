5b9f63f5c75087c39f5984aeca41449d98632735
Doaa Ismael <doaa.ismael@zyda.com>
2022-11-24 00:50:30 +0200

    Time: 1165 ms (71.72%), Space: 27.8 MB (79.14%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -6,8 +6,9 @@ class Solution:
         for n in nums:
             curSum += n
             maxSum = max(maxSum, curSum)
-            if curSum < 0:
+            if curSum <= 0:
                 curSum = 0
+            
         
         
         return maxSum
