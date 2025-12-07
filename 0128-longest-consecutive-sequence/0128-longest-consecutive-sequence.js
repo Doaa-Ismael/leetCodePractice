64400afc5e0bab3330f106bd84509f3a5c7a4eed
Doaa Ismael <doaa.ismael@zyda.com>
2025-12-07 20:42:05 +0200

    Time: 37 ms (68.35%), Space: 80.4 MB (39.4%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -4,7 +4,6 @@
  */
 var longestConsecutive = function(nums) {
     const numsSet = new Set()
-    let count = 0
     let i = 0
     let res = 0
 
@@ -12,13 +11,11 @@ var longestConsecutive = function(nums) {
 
     for(num of numsSet) {
         if(!numsSet.has(num - 1)){
-            count = 1
-            let x = num
-           while(numsSet.has(x + 1)){
-            count++
-            x = x+1
+            let x = num + 1
+           while(numsSet.has(x)){
+            x = x + 1
            }
-           res = Math.max(count, res)
+           res = Math.max(x - num, res)
         }
         
     }
