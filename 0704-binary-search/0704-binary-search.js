da4f6ed267a28e5cecb91a26349c7a6ec6622b6a
Doaa Ismael <doaa.ismael@zyda.com>
2025-12-24 11:35:03 +0200

    Time: 0 ms (100%), Space: 58.1 MB (30.77%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -3,7 +3,7 @@
 function binarySearch (nums, target, l, r) {
     if(r < l) return -1
     if(l == r) return target === nums[l] ? l : -1
-    const mid = Math.floor((r - l) / 2) + l
+    const mid = Math.floor((l + r) / 2)
     if(target === nums[mid]) return mid
     else if (target < nums[mid]) return binarySearch(nums, target, l, mid - 1)
     else return binarySearch(nums, target, mid + 1, r)
