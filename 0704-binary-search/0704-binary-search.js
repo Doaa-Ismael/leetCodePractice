b7852191473c250c7d67003f220964767e7a35ac
Doaa Ismael <doaa.ismael@zyda.com>
2025-12-24 11:30:15 +0200

    Time: 0 ms (100%), Space: 58.4 MB (21.5%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,22 @@
+
+
+function binarySearch (nums, target, l, r) {
+    if(r < l) return -1
+    if(l == r) return target === nums[l] ? l : -1
+    const mid = Math.floor((r - l) / 2) + l
+    if(target === nums[mid]) return mid
+    else if (target < nums[mid]) return binarySearch(nums, target, l, mid - 1)
+    else return binarySearch(nums, target, mid + 1, r)
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+    const result = binarySearch(nums, target, 0, nums.length - 1)
+    return result
+    
+};
