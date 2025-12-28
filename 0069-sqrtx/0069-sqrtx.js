2b66b90e4c1e35a2965037446eab7adea554bd24
Doaa Ismael <doaa.ismael@zyda.com>
2025-12-28 10:27:11 +0200

    Time: 157 ms (5.07%), Space: 65.7 MB (5.07%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let l = 1, r = x - 1
+    if(x == 1 || x === 0) return x
+    while(l <= r) {
+        let mid =  Math.floor((l+r) / 2)
+        console.log({mid, l, r})
+        if (mid*mid == x) return mid
+        else if (mid*mid < x && ((mid+1) * (mid + 1)) > x ) return mid
+        else if (mid*mid > x) r = mid - 1
+        else if (mid*mid < x ) l = mid + 1
+    }
+    
+};
