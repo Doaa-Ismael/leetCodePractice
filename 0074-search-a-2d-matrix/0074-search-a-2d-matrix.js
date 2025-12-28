be668cfb2081a78d8b05234b824ddf19187b87f3
Doaa Ismael <doaa.ismael@zyda.com>
2025-12-28 21:12:12 +0200

    Time: 0 ms (100%), Space: 54 MB (75.03%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -6,14 +6,20 @@
 var searchMatrix = function(matrix, target) {
     let l = 0, r = matrix.length - 1, colsCount = matrix[0].length - 1
     let rowIndex
-    for (let i = 0; i < matrix.length ; i++) {
-        if(matrix[i][0]  <= target && matrix[i][colsCount] >= target) {
-            rowIndex = i
-            break;
+    while(l <= r) {
+        const mid = Math.floor((r + l) / 2)
+        const numAtMid = matrix[mid][0]
+        if(numAtMid == target) return true
+        if(numAtMid < target)  {
+            l = mid + 1
         }
-
+        else {
+            r = mid - 1
+        }
+        if(r <= l ) rowIndex = r
+        else rowIndex = l
     }
-    if(rowIndex == null) return false
+    if(rowIndex == null || rowIndex < 0 || rowIndex > matrix.length) return false
     l = 0, r = matrix[rowIndex].length - 1
     while(l <= r) {
         const mid = Math.floor((r + l) / 2)
