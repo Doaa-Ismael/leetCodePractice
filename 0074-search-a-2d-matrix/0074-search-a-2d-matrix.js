16f994b8c778716190bbb3c919676c619042fc1b
Doaa Ismael <doaa.ismael@zyda.com>
2025-12-28 21:52:11 +0200

    Time: 0 ms (100%), Space: 54.6 MB (37.18%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,30 +4,23 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let l = 0, r = matrix.length - 1, colsCount = matrix[0].length - 1
-    let rowIndex
+    const rowCount = matrix.length
+    const colCount = matrix[0].length
+    let l = 0, r = ( rowCount * colCount ) - 1
     while(l <= r) {
         const mid = Math.floor((r + l) / 2)
-        const numAtMid = matrix[mid][0]
-        if(numAtMid == target) return true
-        if(numAtMid < target)  {
+        const rowInd = Math.floor( mid / colCount )
+        const colInd = mid % colCount
+        const numAtMid = matrix[rowInd][colInd]
+        if (numAtMid == target) return true
+        else if (numAtMid < target) {
             l = mid + 1
         }
         else {
             r = mid - 1
         }
-        if(r <= l ) rowIndex = r
-        else rowIndex = l
     }
-    if(rowIndex == null || rowIndex < 0 || rowIndex > matrix.length) return false
-    l = 0, r = matrix[rowIndex].length - 1
-    while(l <= r) {
-        const mid = Math.floor((r + l) / 2)
-        const numAtMid = matrix[rowIndex][mid]
-        if(numAtMid == target) return true
-        else if (numAtMid < target) l = mid + 1
-        else r = mid - 1
-    }
+
     return false
     
 };
