208a373675f47c5c819ffb3251be4c255e4547c2
Doaa Ismael <doaa.ismael@zyda.com>
2026-01-06 00:03:24 +0200

    Time: 0 ms (100%), Space: 53 MB (96.51%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -7,7 +7,7 @@ var findMin = function(nums) {
     let l = 0, r = nums.length - 1, ans = Number.MAX_SAFE_INTEGER
     while(l <= r) {
         const mid = l + Math.floor((r - l) / 2)
-        if (nums[mid] < nums[r]) {
+        if (nums[mid] < nums[l]) {
             ans = Math.min(ans, nums[mid])
             r = mid
         }
