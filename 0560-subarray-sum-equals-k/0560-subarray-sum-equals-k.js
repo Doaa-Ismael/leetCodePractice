6a4568f68c911ab5dc228c3b68cdb668e382ade0
Doaa Ismael <doaa.ismael@zyda.com>
2026-01-09 18:51:25 +0200

    Time: 42 ms (28.62%), Space: 68.8 MB (5.32%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,19 +4,17 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    let sum = 0
-    let prefix = [0]
-    for (let i = 0; i < nums.length ; i++) {
-        prefix[i+1] = prefix[i] + nums[i]
+    let sum = 0, subArraySum = 0
+    let prefixSumMap = {
+        0: 1
     }
-    for (let i = 0; i < nums.length ; i++) {
-        let subArraySum = 0
-        for (let j = i; j < nums.length; j++) {
-            subArraySum = prefix[j+1] - prefix[i]
-            if(subArraySum === k) {
-                sum++
-            }
+    for (let i = 0; i < nums.length; i++) {
+        subArraySum += nums[i]
+        let restSum = subArraySum - k
+        if(prefixSumMap[restSum]) {
+            sum += prefixSumMap[restSum]
         }
+        prefixSumMap[subArraySum] = (prefixSumMap[subArraySum] || 0) + 1
     }
     return sum 
     
