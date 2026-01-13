a70d7e7ab25c7379707163ee6293f570b5ad6eca
Doaa Ismael <doaa.ismael@zyda.com>
2026-01-13 19:27:42 +0200

    Time: 4 ms (35%), Space: 64.7 MB (86.58%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let profit = 0, minPrice = prices[0]
+    for (let i = 1; i < prices.length ; i++) {
+       if(prices[i] < minPrice)
+            minPrice = prices[i]
+        profit = Math.max(profit, prices[i] - minPrice)
+    }
+    return profit
+};
