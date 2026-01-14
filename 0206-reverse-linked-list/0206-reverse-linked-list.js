3175af9824ebc4e8fe7053fe004fd4c3afeb9dda
Doaa Ismael <doaa.ismael@zyda.com>
2026-01-14 10:30:03 +0200

    Time: 0 ms (100%), Space: 57.6 MB (53.18%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(!head) return null
+    let prev = head, cur = head.next
+    head.next = null
+    while(cur != null) {
+        const nextNode = cur.next
+        cur.next = prev
+        prev = cur
+        cur = nextNode
+    }
+    return prev
+};
